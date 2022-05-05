80ce1afb61037af456326f91da91bb539b86f07c
Pius6 <97384212+Pius6@users.noreply.github.com>
2022-05-05 14:40:48 +0100

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🎬 Updates', url='https://t.me/Movie_Collections_Bot')
+            InlineKeyboardButton('🎬 Updates', url='https://t.me/NetflixHDserie')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
