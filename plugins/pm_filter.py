9cf4c9001df4baec35a82cec8af464e567664c1c
blealex <98812061+blealex@users.noreply.github.com>
2022-05-18 06:09:56 +0100

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -669,14 +669,14 @@ async def auto_filter(client, msg, spoll=False):
              InlineKeyboardButton(text="NEXT ⏭️", callback_data=f"next_{req}_{key}_{offset}")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="🍿 JOIN OUR SERIES CHANNEL 🍿",url="https://t.me/+YqsbYu4L9NExNDI0")]
+            [InlineKeyboardButton(text="🍿 JOIN OUR SERIES CHANNEL 🍿",url="https://t.me/+irawY-tWxtRlNDg8")]
         )
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="🍿 JOIN OUR SERIES CHANNEL 🍿",url="https://t.me/+YqsbYu4L9NExNDI0")]
+            [InlineKeyboardButton(text="🍿 JOIN OUR SERIES CHANNEL 🍿",url="https://t.me/+irawY-tWxtRlNDg8")]
         )
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
