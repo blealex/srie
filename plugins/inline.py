775123c08a61eaeaa9f9bd608fa757cfd84d3151
blealex <98812061+blealex@users.noreply.github.com>
2022-06-01 05:24:43 +0100

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat='')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
